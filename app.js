89e03a7a0bf401ce7f8462a5b931571d238df0f7
Zouheir Layine <zolay1996@gmail.com>
2021-02-23 11:15:04 +0100

    added https

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const http = require('http');
+const http = require('https');
 const socketio = require('socket.io');
 const fs = require('fs');
 
